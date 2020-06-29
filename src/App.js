86b98dbf424956342f1f39c8ad834733a2ae32ba
Carlo Estrada <lcrestrada.dev@gmail.com>
2020-06-29 11:11:53 +0800

    update our menu-items to navigate our withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
 
 import './App.css';
 
+const PastasPage = () => (
+  <div>
+    <h1>PASTAS PAGE</h1>
+  </div>
+)
+
+const MainsPage = () => (
+  <div>
+    <h1>MAINS PAGE</h1>
+  </div>
+)
+
+const DessertsPage = () => (
+  <div>
+    <h1>DESSERTS PAGE</h1>
+  </div>
+)
+
+const ReadytoCookPage = () => (
+  <div>
+    <h1>READY TO COOK PAGE</h1>
+  </div>
+)
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/pastas' component={PastasPage} />
+        <Route path='/mains' component={MainsPage} />
+        <Route path='/desserts' component={DessertsPage} />
+        <Route path='/readytocook' component={ReadytoCookPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,22 +11,26 @@ class Directory extends React.Component {
                 {
                     title: "Pastas",
                     imageUrl: "https://images.unsplash.com/photo-1473093226795-af9932fe5856?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1885&q=80",
-                    id: 1
+                    id: 1,
+                    linkUrl: 'pastas'
                 },
                 {
                     title: "Mains",
                     imageUrl: "https://images.unsplash.com/photo-1577271141104-b6bd7b76b8e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80",
-                    id: 2
+                    id: 2,
+                    linkUrl: 'mains'
                 },
                 {
                     title: "Desserts",
                     imageUrl: "https://i2.wp.com/thenotsocreativecook.com/wp-content/uploads/2013/09/thenotsocreativecook-lecheflan3.jpg",
-                    id: 3
+                    id: 3,
+                    linkUrl: 'desserts'
                 },
                 {
                     title: "Ready to Cook",
                     imageUrl: "https://images.unsplash.com/photo-1571067224158-622a54542fed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80",
-                    id: 4
+                    id: 4,
+                    linkUrl: 'readytocook'
                 }
             ]
         }
@@ -36,8 +40,8 @@ class Directory extends React.Component {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title, imageUrl, id}) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl}/>
+                    this.state.sections.map(({id, ...otherSectionProps}) => (
+                        <MenuItem key={id} {...otherSectionProps}/>
                     ))
                 }
             </div>
